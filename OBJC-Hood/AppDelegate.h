cf3e7aa75d6ea67c317ae4e50e92da7643a1f64c
Christopher Augg <chrisa39@uw.edu>
2016-03-02 14:48:35 -0800

    Initial Commit

---
+++ OBJC-Hood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OBJC-Hood
+//
+//  Created by Chris Augg on 3/2/16.
+//  Copyright © 2016 Auggie Doggie iOSware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBJC-Hood/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OBJC-Hood
+//
+//  Created by Chris Augg on 3/2/16.
+//  Copyright © 2016 Auggie Doggie iOSware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
